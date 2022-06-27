804d47237e84d7f49e8d3cd67159bc47a0e04b4d
Stefan Geißelsöder <stefan.geisselsoeder@hs-ansbach.de>
2022-06-27 16:20:41 +0200

    Working dummy example

--- main.py
+++ main.py
@@ -1,53 +1,69 @@
 import numpy as np
 from sklearn.neural_network import MLPClassifier
+from sklearn.tree import DecisionTreeClassifier
 import matplotlib.pyplot as plt
 import copy
 
 
-def occlusion_sensitivity(data: np.array, model, class_index: int):
-    number_features = data.shape[0]
+def occlusion_sensitivity(model, data: np.array, class_index: int, change_factor=0.0):
+    # Checks the influence of all features on the output
+    # Assumes examples of shape (1, number_of_features)
+    # Reasonable values for change factor would be e.g. 0.0, 0.8, 1.25
 
-    data = np.reshape(data, (1, -1))
+    # Obtain output for actual input
     default_result = model.predict_proba(data)
     default_result_target = default_result[0][class_index]
 
-    modified_data = copy.deepcopy(data)
     occlusion_results = np.zeros(data.shape)
-
-    for channel in range(number_features):
+    for feature in range(data.shape[1]):
+        # Modify input data
         modified_data = copy.deepcopy(data)
-        modified_data[0][channel] = 0.0
+        modified_data[0][feature] *= change_factor
+
+        # Obtain output for modified input
         result = model.predict_proba(modified_data)
         class_result = result[0][class_index]
+
+        # Store the difference
         difference = class_result - default_result_target
-        occlusion_results[0][channel] = difference
+        occlusion_results[0][feature] = difference
     return occlusion_results
 
 
 def get_features_and_labels():
+    # Returns dummy features and labels
     number_examples = 1000
-    # 100 examples, 30 features 30, 1 label
-    features = np.random.randint(0, 100, size=(number_examples, 30))
     number_classes = 10
+    # Create random features and labels
+    features = np.random.randint(0, 100, size=(number_examples, 30))
     labels = np.arange(0.0, number_classes, number_classes/number_examples)
+    # Make one of the features highly correlated with the labels
     features[:, 28] = labels * 10.0 + 12.5
-    return features, np.reshape(labels, (-1, 1)).astype(int)
+    return features, labels.astype(int)
 
 
+# Setup data
 X, y = get_features_and_labels()
-model = MLPClassifier(hidden_layer_sizes=(20,), max_iter=400)
+# Train model
+# model = MLPClassifier(hidden_layer_sizes=(10,), max_iter=400)
+model = DecisionTreeClassifier()
 model.fit(X, y)
-# result = model.predict(X)
 
-# sensitivity_example = np.reshape(X[-1], (1, -1))
-sensitivity_example = X[-1]
-occlusion_result = occlusion_sensitivity(model=model, data=sensitivity_example, class_index=0)
-# occlusion_result_image = np.reshape(occlusion_result, (1, occlusion_result.shape[1]))
+# Test multiple examples
+for example_test in range(2):
+    # Pick a random example
+    random_example = int(np.random.random() * y.shape[0])
+    print("Evaluating random example at index", random_example)
+    sensitivity_example = np.reshape(X[random_example], (1, -1))
+    true_class = int(y[random_example])
 
-fig, axs = plt.subplots(2, sharex=True)
-axs[0].set_aspect("auto")
-axs[0].imshow(occlusion_result)
-axs[1].plot(sensitivity_example)
+    # Test sensitivity of result against occlusion
+    occlusion_result = occlusion_sensitivity(model=model, data=sensitivity_example, class_index=true_class)
 
-plt.tight_layout()
-plt.show()
+    # plot result
+    fig, axs = plt.subplots(2, sharex=True)
+    axs[0].set_aspect("auto")
+    axs[0].imshow(occlusion_result)
+    axs[1].plot(X[-1])
+    plt.tight_layout()
+    plt.show()
